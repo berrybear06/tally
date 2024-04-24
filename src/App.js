96aee101ca3b8b2ebccf0f3fb81d35dcb6949e19
berrybear06 <79681277+berrybear06@users.noreply.github.com>
2024-04-24 11:05:28 -0700

    Footer and redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './Home';
 import Addition from './Addition';
 
@@ -9,6 +9,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/addition" element={<Addition />} />
+				<Route path="*" element={<Navigate to="/" />} />
 			</Routes>
 		</Router>
 	);
