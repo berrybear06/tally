feb5462ba69fa085f68ed6b82b810ebc03b64c35
berrybear06 <79681277+berrybear06@users.noreply.github.com>
2024-04-30 18:05:26 -0700

    Cross out incorrect answers

--- src/AnswerChoices.js
+++ src/AnswerChoices.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import './AnswerChoices.css';
+import XMark from './assets/icons/x-mark.png';
 
 const AnswerChoices = ({ answerList, correctAnswer, selectedAnswer, clickHandler }) => {
 	return (
 		<div className="d-flex justify-content-center mt-3 mx-auto text-center answer-choice-row">
 			{answerList.map((answer, index) => (
-				<button
-					key={index}
-					className={`prompt-size-1 btn btn-lg mx-2 ${selectedAnswer !== null ? (answer === correctAnswer ? 'btn-success' : 'btn-danger') : 'btn-primary'}`}
-					onClick={() => clickHandler(answer)}
-				>
-					{answer}
-				</button>
+				<div key={index} className="position-relative">
+					<button
+						className={`prompt-size-1 btn btn-lg mx-2 btn-primary ${
+							selectedAnswer !== null 
+							&& (answer === correctAnswer ? 'button-correct' : 'button-incorrect') 
+							}`}
+						onClick={() => clickHandler(answer)}
+					>
+						{answer}
+					</button>
+					
+					{selectedAnswer !== null && answer !== correctAnswer && (
+						<img
+							src={XMark}
+							alt="Incorrect"
+							className="x-mark"
+						/>
+					)}
+				</div>
 			))}
 		</div>
 	);
